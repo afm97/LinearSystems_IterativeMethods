ef378247ce72c0ed234a766dc8078558fe3c01ae
afm97 <arthurfranca97@gmail.com>
2023-12-05 16:31:22 -0300

    Alteracao no formato de impressao: agora imprime o array inteiro em uma linha

--- printArray.h
+++ printArray.h
@@ -2,6 +2,7 @@ void printArray(unsigned int rows, double array[rows])
 {
     for (int i = 0; i < rows; i++)
     {
-        printf("%f  \n", array[i]);
+        printf("%f  ", array[i]);
     }
+    printf("\n");
 }
